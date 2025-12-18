6fc44e29d961ccc9e6b8fa015e1c1f50000f30ab
Mr_ J <javad.seyyedi20006@gmail.com>
2025-12-18 17:55:10 +0330

    Refactor Button base component
    Rename `button` class name to `btn`
    Add additional props for flexability

--- src/components/base/Button.jsx
+++ src/components/base/Button.jsx
@@ -1,9 +1,20 @@
-const Button = ({ children, onClick = null }) => {
-	return (
-		<button onClick={onClick} className="button">
-			{children}
-		</button>
-	);
+const Button = ({
+  children,
+  onClick,
+  className = "",
+  type = "button",
+  ...rest
+}) => {
+  return (
+    <button
+      type={type}
+      className={`btn ${className}`.trim()}
+      onClick={onClick}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
